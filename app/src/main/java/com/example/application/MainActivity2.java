793ff69deb3f66e29d90c48c3cf81bb8cd23d682
201102122 <d.j.cl3m0n5@gmail.com>
2023-05-30 13:54:23 -0700

    SocialSync ~Added Registration Page ~Updated Sign In Page

--- app/src/main/java/com/example/application/MainActivity2.java
+++ app/src/main/java/com/example/application/MainActivity2.java
@@ -3,6 +3,11 @@ package com.example.application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        EditText username = (EditText) findViewById(R.id.username_et);
+
+        MaterialButton regbtn = (MaterialButton) findViewById(R.id.register_btn);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username1 = username.getText().toString();
+                Toast.makeText(MainActivity2.this, "Username is", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
