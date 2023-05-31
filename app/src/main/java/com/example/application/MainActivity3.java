05fac9971931a44e1668534f40cb8420977d0ece
201102122 <d.j.cl3m0n5@gmail.com>
2023-05-31 07:20:31 -0700

    SocialSync ~Added Main Page ~Finished Registration & Login Page ~Adding Tabs to scroll through

---
+++ app/src/main/java/com/example/application/MainActivity3.java
@@ -0,0 +1,36 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity3 extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        button = findViewById(R.id.enter_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
+
